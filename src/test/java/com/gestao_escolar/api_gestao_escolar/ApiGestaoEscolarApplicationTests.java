432fa218f7293b26f8114065090721ea827e16bd
Ian Gurgel <iangurgel25@hotmail.com>
2026-01-18 23:34:24 -0300

    first commit

---
+++ src/test/java/com/gestao_escolar/api_gestao_escolar/ApiGestaoEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestao_escolar.api_gestao_escolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestaoEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
